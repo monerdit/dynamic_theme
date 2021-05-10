de6c5707ed37d429c23ee701eed3f9cc3ebafe28
monerdit <monerdit@gmail.com>
2021-05-10 16:21:45 +0430

    updated for flutter v2

---
+++ test/easy_theme_test.dart
@@ -0,0 +1,54 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+ValueKey<String> key = const ValueKey<String>('ok');
+DynamicThemeState state;
+GlobalKey<DynamicThemeState> easyThemeKey = GlobalKey<DynamicThemeState>();
+
+void main() {
+  testWidgets('test finds state', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    expect(state, equals(null));
+
+    await tester.tap(find.byKey(key));
+    await tester.pump();
+
+    expect(state, isNotNull);
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DynamicTheme(
+        key: easyThemeKey,
+        defaultBrightness: Brightness.light,
+        data: (Brightness brightness) {
+          return ThemeData(
+            primarySwatch: Colors.indigo,
+            brightness: brightness,
+          );
+        },
+        themedWidgetBuilder: (BuildContext context, ThemeData theme) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: theme,
+            home: ButtonPage(),
+          );
+        });
+  }
+}
+
+class ButtonPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        state = DynamicTheme.of(context);
+      },
+      key: key,
+    );
+  }
+}
---
+++ test/easy_theme_test_change.dart
@@ -0,0 +1,65 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+ValueKey<String> key = const ValueKey<String>('ok');
+
+GlobalKey<DynamicThemeState> easyThemeKey = GlobalKey<DynamicThemeState>();
+
+void main() {
+  testWidgets('change brightness', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    MaterialApp app = find.byType(MaterialApp).evaluate().first.widget;
+    expect(app.theme.brightness, equals(Brightness.dark));
+
+    await tester.tap(find.byKey(key));
+    await tester.pumpAndSettle();
+
+    app = find.byType(MaterialApp).evaluate().first.widget;
+    expect(app.theme.brightness, equals(Brightness.light));
+
+    await tester.tap(find.byKey(key));
+    await tester.pumpAndSettle();
+
+    app = find.byType(MaterialApp).evaluate().first.widget;
+    expect(app.theme.brightness, equals(Brightness.dark));
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DynamicTheme(
+        key: easyThemeKey,
+        defaultBrightness: Brightness.dark,
+        data: (Brightness brightness) {
+          return ThemeData(
+            primarySwatch: Colors.indigo,
+            brightness: brightness,
+          );
+        },
+        themedWidgetBuilder: (BuildContext context, ThemeData theme) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: theme,
+            home: ButtonPage(),
+          );
+        });
+  }
+}
+
+class ButtonPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        DynamicTheme.of(context).setBrightness(
+            Theme.of(context).brightness == Brightness.dark
+                ? Brightness.light
+                : Brightness.dark);
+      },
+      key: key,
+    );
+  }
+}
